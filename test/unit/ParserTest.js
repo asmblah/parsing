c8929953982aad3dd50f223bba638983151fa5c9
Dan Phillimore <dan@ovms.co>
2021-03-12 19:17:39 +0000

    Move custom rules option test out to a separate file

--- test/unit/ParserTest.js
+++ test/unit/ParserTest.js
@@ -853,54 +853,6 @@ describe('Parser', function () {
             });
         });
 
-        describe('custom rules option', function () {
-            it('should support overriding a rule with a new one that references the original', function () {
-                var grammarSpec = {
-                        ignore: 'whitespace',
-                        rules: {
-                            'go_statement': {
-                                components: [{what: /go/, allowMerge: false}]
-                            },
-                            'end_statement': {
-                                components: [{what: /end/, allowMerge: false}]
-                            },
-                            'whitespace': /\s+/,
-                            'single_statement': {
-                                components: {oneOf: ['go_statement', 'end_statement']}
-                            },
-                            'statement': {
-                                components: ['single_statement', /;/]
-                            },
-                            'program': {
-                                components: {name: 'statements', zeroOrMoreOf: 'statement'}
-                            }
-                        },
-                        start: 'program'
-                    },
-                    options = {
-                        rules: {
-                            'do_something_statement': {
-                                components: [{what: /do_something_custom/, allowMerge: false}]
-                            },
-                            'single_statement': {
-                                // Override `single_statement`, but refer back to the original
-                                components: {oneOf: ['do_something_statement', 'single_statement']}
-                            }
-                        }
-                    },
-                    parser = new Parser(grammarSpec, null, options);
-
-                expect(parser.parse('go; do_something_custom; end;')).to.deep.equal({
-                    name: 'program',
-                    statements: [
-                        {name: 'go_statement'},
-                        {name: 'do_something_statement'},
-                        {name: 'end_statement'}
-                    ]
-                });
-            });
-        });
-
         describe('custom start rule', function () {
             it('should support specifying a different rule to start parsing from', function () {
                 var grammarSpec = {
